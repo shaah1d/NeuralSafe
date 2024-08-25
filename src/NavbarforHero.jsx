420d88af8c8eb43b3563c1e5025b8d463c66012f
Shaahid <shaahid2453@gmail.com>
2024-08-25 03:39:42 -0700

    Update NavbarforHero.jsx

--- src/NavbarforHero.jsx
+++ src/NavbarforHero.jsx
@@ -22,9 +22,9 @@ function NavbarforHero() {
         <ul
           tabIndex={0}
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li><a>Homepage</a></li>
-          <li><a>Portfolio</a></li>
-          <li><a>About</a></li>
+          <li><a href="/">Homepage</a></li>
+        
+          <li><a href="/about">About</a></li>
         </ul>
       </div>
     </div>
